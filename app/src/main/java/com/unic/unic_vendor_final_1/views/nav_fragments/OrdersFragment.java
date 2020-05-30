a4aeca9aad17dbbe8e41434e4fe45a12a16d5588
nisarg2104 <nisarg2104@gmail.com>
2020-05-30 21:22:13 +0530

    scrollrefreshlayout added

--- app/src/main/java/com/unic/unic_vendor_final_1/views/nav_fragments/OrdersFragment.java
+++ app/src/main/java/com/unic/unic_vendor_final_1/views/nav_fragments/OrdersFragment.java
@@ -7,6 +7,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,8 +41,8 @@ public class OrdersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view=inflater.inflate(R.layout.fragment_my_orders, container, false);
-        rvOrders=view.findViewById(R.id.rvAllOrders);
+        myOrdersBinding=FragmentMyOrdersBinding.inflate(getLayoutInflater(),container,false);
+        rvOrders=myOrdersBinding.rvAllOrders;
         userShopsViewModel=new ViewModelProvider(getActivity()).get(UserShopsViewModel.class);
         allOrdersAdapter=new AllOrdersAdapter(getActivity());
         rvOrders.setAdapter(allOrdersAdapter);
@@ -56,11 +57,18 @@ public class OrdersFragment extends Fragment {
 
             }
         });
+        myOrdersBinding.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //Update here.
+            }
+        });
+
 
 
 
         // Inflate the layout for this fragment
-        return view;
+        return myOrdersBinding.getRoot();
     }
 
 
